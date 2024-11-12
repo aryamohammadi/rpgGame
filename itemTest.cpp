152915b50cecf5946d40dc5195f0674f65bee926
macos052 <macos052@ucr.edu>
2024-11-12 13:51:31 -0800

    modified unit test

--- itemTest.cpp
+++ itemTest.cpp
@@ -1,10 +1,12 @@
 #include "item.h"
 #include "gtest/gtest.h"
-
-TEST(ItemTest, emptyName){
+#include <sstream>
+using std::ostringstream;
+TEST(ItemTest, outputItem){
     Item A(Item::ItemType::WEAPON, "Emily",Item::Grade::COMMON,"hi");
+    ostringstream out;
+    out << A;
     
-    string result = A.getName();
-    
-    EXPECT_EQ(result, "Emily");
+    string expect = "Name: " + A.getName() + "\n";
+    EXPECT_EQ(out, "");
 }

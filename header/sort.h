8d0a430f024689f9034acc478694e268a180aa53
macos052 <macos052@ucr.edu>
2024-11-15 07:47:23 -0800

    Implemented AbstractSort

--- header/sort.h
+++ header/sort.h
@@ -1,5 +1,26 @@
 #pragma once
 #define SORT_H
+#include "../header/itemStack.h"
+enum class SortOrder{
+    Ascending,
+    Descending
+};
+template<typename T>
+class AbstractSort{
+
+    public:
+        virtual void sort(vector<itemStack*>& array, SortOrder order = SortOrder::Ascending) = 0;
+        bool isSorted(const vector<itemStack*>& array, SortOrder order = SortOrder::Ascending) const {
+            for(unsigned i = 1; i < array.size(); i++){
+                if((order == SortOrder::Ascending && array[i]->item < array[i - 1]->item) ||(order == SortOrder::Descending && array[i] > array[i - 1])){
+                    return false;
+                }
+            }
+            return true;
+        }
+};
+#pragma once
+#define SORT_H
 template<typename T>
 class AbstractSort{
 

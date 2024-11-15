74c284c06ab20c2214f3d40e1449c516b9894b95
macos052 <macos052@ucr.edu>
2024-11-15 10:37:30 -0800

    remove unneccesary code

--- header/sort.h
+++ header/sort.h
@@ -19,23 +19,3 @@ class AbstractSort{
             return true;
         }
 };
-#pragma once
-#define SORT_H
-template<typename T>
-class AbstractSort{
-
-    public:
-        enum class SortOrder{
-            Ascending,
-            Descending
-        };
-        virtual void sort(vector<T>& array, SortOrder order = SortOrder::Ascending) = 0;
-        bool isSorted(const vector<T>& array, SortOrder order = SortOrder::Ascending) const {
-            for(unsigned i = 1; i < array.size(); i++){
-                if((order == SortOrder::Ascending && array[i] < array[i - 1]) ||(order == SortOrder::Descending && array[i] > array[i - 1])){
-                    return false;
-                }
-            }
-            return true;
-        }
-};

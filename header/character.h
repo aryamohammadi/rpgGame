8881ad6f96ec3fb40ddd8e0111b1607804f9981a
macos052 <macos052@ucr.edu>
2024-11-15 20:07:16 -0800

    rename a few things to make it more readible in the context

--- header/character.h
+++ header/character.h
@@ -16,7 +16,7 @@ class Character{
         Character(string name) : characterName(name), inventoryOfCharacter(), health(100),damage(0),defense(0),isDead(false){} 
 
         void setHealth(int healthOfCharacter){ health = healthOfCharacter; }
-        void setAttack(int attackOfCharacter){ damage = attackOfCharacter; }
+        void setDamage(int damageOfCharacter){ damage = damageOfCharacter; }
         void takeDamage(int damageOnCharacter){ health-= damageOnCharacter; }
         virtual void attack() = 0;
         virtual void defend() = 0;

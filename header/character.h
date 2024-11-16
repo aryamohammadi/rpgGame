cd4d82db3b1590303b186c2b6badd19b654d637d
jgarc826 <jgarc826@ucr.edu>
2024-11-15 16:13:08 -0800

    created character class

---
+++ header/character.h
@@ -0,0 +1,24 @@
+#pragma once
+#define CHARACTER_H
+#include "inventory.h"
+
+class Character{
+    private:
+        std::string characterName;
+        Inventory inventoryOfCharacter;
+        int health;
+        int attack;
+        int defense;
+        bool isDead;
+        
+
+    public:
+        Character(string name) : characterName(name), inventoryOfCharacter(), health(100),attack(0),defense(0),isDead(false){} 
+
+        void setHealth(int healthOfCharacter){ health = healthOfCharacter; }
+        void setAttack(int attackOfCharacter){ attack = attackOfCharacter; }
+        void takeDamage(int damageOnCharacter){ health-= damageOnCharacter; }
+        virtual void attack() = 0;
+        virtual void defend() = 0;
+
+};

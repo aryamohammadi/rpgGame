dca5e2f0f966e502d949c1e371dcf6ed694a5fe0
macos052 <macos052@ucr.edu>
2024-11-16 11:41:37 -0800

    Added a test for sort

--- Tests/HungrySortTest.cpp
+++ Tests/HungrySortTest.cpp
@@ -1,7 +1,16 @@
 #include "../header/item.h"
 #include "../header/inventory.h"
+#include "../header/sort.h"
 #include <sstream>
 using std::ostringstream;
 using testing::Return;
 using namespace std;
 
+TEST(SortTest, isSortedOneItemTest){
+    MockAbstractItemSort s(CompareItem::CompareBy::Name);
+    vector<ItemStack*> stacks = {new ItemStack(new MockItem(Item::ItemType::POTION, "Emilly"))};
+
+    EXPECT_TRUE(s.isSorted(stacks,SortOrder::Ascending, CompareItem::CompareBy::Name));
+
+
+}

f0876333156b90555c33dd26ce7f4b144bf240f1
macos052 <macos052@ucr.edu>
2024-11-16 11:43:41 -0800

    Added predeclaration to parameters

--- header/sort.h
+++ header/sort.h
@@ -2,17 +2,15 @@
 #define SORT_H
 #include "../header/itemStack.h"
 #include "../header/compare.h"
-enum class SortOrder{
-    Ascending,
-    Descending
-};
+#include "../header/sortorder.h"
 class AbstractItemSort{
     protected:
     CompareItem::CompareBy sortConfig;
     public:
         AbstractItemSort(CompareItem::CompareBy sortMode):sortConfig(sortMode){}
-        virtual void sort(vector<ItemStack*>& array, SortOrder order = SortOrder::Ascending, CompareItem::CompareBy mode) = 0;
-        bool isSorted(const vector<ItemStack*>& array, SortOrder order = SortOrder::Ascending, CompareItem::CompareBy mode) const {
+        virtual void sort(vector<ItemStack*>& array, SortOrder order = SortOrder::Ascending, CompareItem::CompareBy mode = CompareItem::CompareBy::Name) = 0;
+       // In the header file (e.g., sort.h)
+        bool isSorted(const std::vector<ItemStack*>& array, SortOrder order = SortOrder::Ascending, CompareItem::CompareBy mode = CompareItem::CompareBy::Name) const{
             for(unsigned i = 1; i < array.size(); i++){
                 if((order == SortOrder::Ascending && CompareItem::compare(array[i]->getItem(), array[i - 1]->getItem(), mode, SortOrder::Descending)) ||(order == SortOrder::Descending && CompareItem::compare(array[i]->getItem(), array[i - 1]->getItem(), mode, SortOrder::Ascending))){
                     return false;
@@ -20,9 +18,11 @@ class AbstractItemSort{
             }
             return true;
         }
+
 };
 
 class MockAbstractItemSort : public AbstractItemSort{
     public:
+        MockAbstractItemSort(CompareItem::CompareBy sortMode):AbstractItemSort(sortMode){}
         MOCK_METHOD(void, sort, (vector<ItemStack*>& array, SortOrder order, CompareItem::CompareBy mode), (override));
 };

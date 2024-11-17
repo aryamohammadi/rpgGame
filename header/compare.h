804a6d80a4074d900c3a1592b38084da435b7afa
macos052 <macos052@ucr.edu>
2024-11-17 11:09:33 -0800

    Accomated to deal with time fixes

--- header/compare.h
+++ header/compare.h
@@ -3,6 +3,7 @@
 #include "../header/sortorder.h"
 #include "../header/item.h"
 #include "../header/compareItemType.h"
+#include <cmath>
 class CompareItem{
     public:
         enum CompareBy{
@@ -15,34 +16,34 @@ class CompareItem{
         static bool compare(const Item* lowerBound, const Item* upperBound, CompareBy compareBy, SortOrder order = SortOrder::Ascending, CompareItemTypeCharacteristics::Characteristics attribute = CompareItemTypeCharacteristics::Characteristics::None){
             switch(compareBy){
                 case CompareBy::Grade:
-                    if(order == SortOrder::Descending && upperBound->getGrade() < lowerBound->getGrade()){
+                    if(order == SortOrder::Descending && upperBound->getGrade() <= lowerBound->getGrade()){
                         return true;
                     }
-                    if(order == SortOrder::Ascending && upperBound->getGrade() > lowerBound->getGrade()){
+                    if(order == SortOrder::Ascending && upperBound->getGrade() >= lowerBound->getGrade()){
                         return true;
                     }
                     break;
                 case CompareBy::Type:
-                    if(order == SortOrder::Descending && upperBound->getType() < lowerBound->getType()){
+                    if(order == SortOrder::Descending && upperBound->getType() <= lowerBound->getType()){
                         return true;
                     }
-                    if(order == SortOrder::Ascending && upperBound->getType() > lowerBound->getType()){
+                    if(order == SortOrder::Ascending && upperBound->getType() >= lowerBound->getType()){
                         return true;
                     }
                     break;
                 case CompareBy::Name:
-                    if(order == SortOrder::Descending && upperBound->getName() < lowerBound->getName()){
+                    if(order == SortOrder::Descending && upperBound->getName() <= lowerBound->getName()){
                         return true;
                     }
-                    if(order == SortOrder::Ascending && upperBound->getName() > lowerBound->getName()){
+                    if(order == SortOrder::Ascending && upperBound->getName() >= lowerBound->getName()){
                         return true;
                     }
                     break;
                 case CompareBy::Time:
-                    if(order == SortOrder::Descending && upperBound->getTime() < lowerBound->getTime()){
+                    if(order == SortOrder::Descending && upperBound->getTime() < lowerBound->getTime() || std::fabs(upperBound->getTime() - lowerBound->getTime()) < 1e-6){
                         return true;
                     }
-                    if(order == SortOrder::Ascending && upperBound->getTime() && lowerBound->getTime()){
+                    if(order == SortOrder::Ascending && upperBound->getTime() > lowerBound->getTime() || std::fabs(upperBound->getTime() - lowerBound->getTime()) < 1e-6){
                         return true;
                     }
                     break;

5a08f370e7f21c78ab0422753638e47e7f6df1df
macos052 <macos052@ucr.edu>
2024-11-17 14:01:48 -0800

    simplified logic

--- header/insertionSort.h
+++ header/insertionSort.h
@@ -5,11 +5,11 @@ using namespace std;
 class InsertionSort : public AbstractItemSort{
     public:
         InsertionSort(CompareItem::CompareBy sortMode):AbstractItemSort(sortMode){}
-        void sort(vector<ItemStack*>& array, SortOrder order, CompareItem::CompareBy mode) override{
+        void sort(vector<ItemStack*>& array, SortOrder order) override{
             for(unsigned i = 1; i < array.size(); i++){
                 int j = i - 1;
                 ItemStack* key = array[i];
-                while(j >= 0 && !CompareItem::compare(array[j]->getItem(), key->getItem(), mode, order)){
+                while(j >= 0 && !CompareItem::compare(array[j]->getItem(), key->getItem(), sortConfig, order)){
                     array[j + 1] = array[j];
                     j--;
                 }

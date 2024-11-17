0d9d05ca55c43de768083ea457aca124afc0822d
macos052 <macos052@ucr.edu>
2024-11-16 16:13:37 -0800

    removed space

--- header/insertionSort.h
+++ header/insertionSort.h
@@ -7,7 +7,7 @@ class InsertionSort : public AbstractItemSort{
         void sort(vector<ItemStack*>& array, SortOrder order, CompareItem::CompareBy mode) override{
             for(unsigned i = 1; i < array.size(); i++){
                 int j = i - 1;
-                while( j < array.size() && !CompareItem::compare(array[j + 1]->getItem(), array[j]->getItem(), mode, order)){
+                while(j < array.size() && !CompareItem::compare(array[j + 1]->getItem(), array[j]->getItem(), mode, order)){
                     swap(array[j+1], array[j]);
                     j++;
                 }

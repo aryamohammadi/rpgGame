7be1b9434e93ddefe8b335b092d8e6814f3d16a6
macos052 <macos052@ucr.edu>
2024-11-17 14:01:23 -0800

    Implemented a bit

--- header/bucketSort.h
+++ header/bucketSort.h
@@ -1,9 +1,41 @@
 #pragma once
 #define BUCKETSORT_H
 #include "../header/sort.h"
+#include "../header/compareItemType.h"
 class BucketSort : public AbstractItemSort{
-
+    private:
+        CompareItemTypeCharacteristics::Characteristics attribute;
     public: 
-        BucketSort(CompareItem::CompareBy sortMode):AbstractItemSort(sortMode){}
-        void sort(vector<ItemStack*>& array, SortOrder order, CompareItem::CompareBy mode) override;
+        BucketSort(CompareItem::CompareBy sortMode, CompareItemTypeCharacteristics::Characteristics attribute = CompareItemTypeCharacteristics::Characteristics::None):AbstractItemSort(sortMode), attribute(attribute){}
+        void sort(vector<ItemStack*>& array, SortOrder order) override{
+            if(sortConfig == CompareItem::CompareBy::Type){
+                vector<ItemStack*> weaponsBucket;
+                vector<ItemStack*> potionsBucket;
+                vector<ItemStack*> armourBucket;
+
+                for(ItemStack* stack : array){
+                    if(stack == nullptr){
+                        break;
+                    }
+                    Item::ItemType curr = stack->getItem()->getType();
+                    switch(curr){
+                        case Item::ItemType::WEAPON:
+                            weaponsBucket.push_back(new ItemStack(*stack));
+                            break;
+                        case Item::ItemType::ARMOUR:
+                            armourBucket.push_back(new ItemStack(*stack));
+                            break;
+                        case Item::ItemType::POTION:
+                            potionsBucket.push_back(new ItemStack(*stack));
+                            break;
+                    }
+                }
+                
+
+            }
+            else{
+                InsertionSort s(sortConfig);
+                s.sort(array, order);
+            }
+        }
 };

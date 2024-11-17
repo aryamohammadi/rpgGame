74df9b2b066a982f635df4363a866e1dc1c073f8
macos052 <macos052@ucr.edu>
2024-11-16 21:57:02 -0800

    changed to const string&

--- header/character.h
+++ header/character.h
@@ -13,7 +13,7 @@ class Character{
         
 
     public:
-        Character(string name) : characterName(name), inventoryOfCharacter(), health(100),damage(0),defense(0),isDead(false){} 
+        Character(const string& name) : characterName(name), inventoryOfCharacter(), health(100),damage(0),defense(0),isDead(false){} 
 
         void setHealth(int healthOfCharacter){ health = healthOfCharacter; }
         void setDamage(int damageOfCharacter){ damage = damageOfCharacter; }

6b23614404559e1813a2a2a8a89d7b03453ab70f
macos052 <macos052@ucr.edu>
2024-11-18 11:58:30 -0800

    Implemented item merge sort

--- header/mergeSort.h
+++ header/mergeSort.h
@@ -4,7 +4,9 @@
 #include "../header/sort.h"
 
 class MergeSort : public AbstractItemSort{
-
+    private:
+        void merge(vector<ItemStack*>& array, SortOrder order, int left, int mid, int right);
+        void mergeSort(vector<ItemStack*>& array, SortOrder order, int left, int right);
     public:
         MergeSort(CompareItem::CompareBy sortMode):AbstractItemSort(sortMode){}
         void sort(vector<ItemStack*>& array, SortOrder order) override;

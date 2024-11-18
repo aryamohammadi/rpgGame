269eea3aab58d75d97c90b9831c4b00a8493bad1
macos052 <macos052@ucr.edu>
2024-11-17 21:28:32 -0800

    Added safeguards

--- header/sort.h
+++ header/sort.h
@@ -11,6 +11,12 @@ class AbstractItemSort{
         virtual void sort(vector<ItemStack*>& array, SortOrder order) = 0;
         bool isSorted(const std::vector<ItemStack*>& array, SortOrder order) const{
             for(unsigned i = 1; i < array.size(); i++){
+                if(array[i] == nullptr){
+                    if(i + 1 < array.size() && array[i + 1] != nullptr){
+                        return false; //any holes 
+                    }
+                    return true; //no holes, so end of array
+                }
                 if(!CompareItem::compare(array[i - 1]->getItem(), array[i]->getItem(), sortConfig, order)){
                     return false;
                 }

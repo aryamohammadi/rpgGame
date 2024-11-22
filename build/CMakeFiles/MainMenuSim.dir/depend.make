b67acb1bea9f5231aca456b6e79ecbbb24c96693
Ryan Jourdain <rjjourdain1@gmail.com>
2024-11-22 15:09:55 -0800

    Delete build directory

--- build/CMakeFiles/MainMenuSim.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MainMenuSim.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/rjour001/final-project-the-computer-enthusiasts
-# Build directory: /home/csmajs/rjour001/final-project-the-computer-enthusiasts/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MainMenuTests "/home/csmajs/rjour001/final-project-the-computer-enthusiasts/build/MainMenuTests")
-set_tests_properties(MainMenuTests PROPERTIES  _BACKTRACE_TRIPLES "/home/csmajs/rjour001/final-project-the-computer-enthusiasts/CMakeLists.txt;35;add_test;/home/csmajs/rjour001/final-project-the-computer-enthusiasts/CMakeLists.txt;0;")
-subdirs("googletest")

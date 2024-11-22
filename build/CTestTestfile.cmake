69400a94951afa937f88b58d820da749d0d0a3bc
Ryan Jourdain <rjjourdain1@gmail.com>
2024-11-22 13:45:07 -0800

    All Unit Tests Passed

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/csmajs/rjour001/final-project-the-computer-enthusiasts
+# Build directory: /home/csmajs/rjour001/final-project-the-computer-enthusiasts/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(DifficultyTests "/home/csmajs/rjour001/final-project-the-computer-enthusiasts/build/DifficultyTests")
+set_tests_properties(DifficultyTests PROPERTIES  _BACKTRACE_TRIPLES "/home/csmajs/rjour001/final-project-the-computer-enthusiasts/CMakeLists.txt;18;add_test;/home/csmajs/rjour001/final-project-the-computer-enthusiasts/CMakeLists.txt;0;")
+subdirs("googletest")

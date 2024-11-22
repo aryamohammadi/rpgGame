38e3ba5f839ace08f80db1aaaaaf8e886bcaf793
Ryan Jourdain <rjjourdain1@gmail.com>
2024-11-22 14:52:46 -0800

    All Unit Tests Passed

--- Main_Menu/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/rjour001/final-project-the-computer-enthusiasts/googletest
-# Build directory: /home/csmajs/rjour001/final-project-the-computer-enthusiasts/Main_Menu/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")

e9b0d041969d3467063767800f16dcab1cb6cf55
Arya Mohammadi <amoha113@ucr.edu>
2024-12-04 16:30:25 -0800

    debugging. commented on issues to come back and fix up.

--- CPP_Files/combat.cpp
+++ CPP_Files/combat.cpp
@@ -12,31 +12,38 @@ Combat::~Combat() = default;
 
 void Combat::startBattle() { // Indenting this was quite nice - Jameel
     MaxHeap heap;
-
+    //comment by arya:
     //FIXME: If fightersAlive is empty, accessing fightersAlive.front() causes undefined behavior. 
     //       Need to discuss if this should be checked beforehand
     //
     bool playerAlive = fightersAlive.front()->isAlive();//boolean checks that if the player in first index is alive
-        while(playerAlive && fightersAlive.size() > 1){// this makes sure that the loop will only run if there are enemies and a playerAlive
-            vector<Character*> turnOrder = fightersAlive;
-            heap.heapsort(turnOrder);
-            for(int i = 0; i < turnOrder.size();i++){
-                if(turnOrder.at(i)->getName().find("Enemy")!= string::npos){
-                    turnOrder.at(i)->attack(*fightersAlive.front());
-                    //attack first index of fightersAlive vector(need to implement this);
-                    if(fightersAlive.at(0)->getHealth() <= 0){//check if player health is not 0. if its 0 set playerAliveToFalse otherwise do nothing
-                        delete fightersAlive.at(0);
-                        playerAlive = false;
-                    }
+    //comment by arya:
+    //FIXME: If fightersAlive.size() > 1 becomes false during execution, playerAlive won't be updated properly. 
+    //       Also, when the player is dead and removed, the logic assumes enemies are still there.
+    //       Need to review to make sure this is correct
+    while(playerAlive && fightersAlive.size() > 1){// this makes sure that the loop will only run if there are enemies and a playerAlive
+        vector<Character*> turnOrder = fightersAlive;
+        heap.heapsort(turnOrder);
+        for(int i = 0; i < turnOrder.size();i++){
+            if(turnOrder.at(i)->getName().find("Enemy")!= string::npos){
+                turnOrder.at(i)->attack(*fightersAlive.front());
+                //attack first index of fightersAlive vector(need to implement this);
+                if(fightersAlive.at(0)->getHealth() <= 0){//check if player health is not 0. if its 0 set playerAliveToFalse otherwise do nothing
+                    delete fightersAlive.at(0);
+                    playerAlive = false;
                 }
-                else {
-                    cout << "Choose an enemy to attack" << endl;
-                    int playerToAttack = playerDecidesWhoToAttack();
-                    fightersAlive.front()->attack(*fightersAlive.at(playerToAttack));
-                    //we need the actual player to decide who he wants to attack enemy1 or w - done
-                    //we have to output a list of valid players - done
-                    //still need to perform attack 
-
+            }
+            else {
+                cout << "Choose an enemy to attack" << endl;
+                int playerToAttack = playerDecidesWhoToAttack();
+                fightersAlive.front()->attack(*fightersAlive.at(playerToAttack));
+                //we need the actual player to decide who he wants to attack enemy1 or w - done
+                //we have to output a list of valid players - done
+                //still need to perform attack 
+                
+                //comment by Arya:
+                //FIXME: isnt this for loop redundant? we already have a for loop in removePlayerFromHeap that
+                //       loops through fightersAlive. 
                 for (int i = 0; i < fightersAlive.size(); ++i) {
                     Character* fighter = fightersAlive[i];
                     if (fighter->getCharacterName() == "Player") {
@@ -65,12 +72,14 @@ int Combat::playerDecidesWhoToAttack(){
     }
     int choiceToAttack;
     bool validChoice = false;
+    //comment by arya:
+    //FIXME: "Invalid Choice." always prints even if the user enters a valid choice. Need to fix.
     while(!(validChoice)){
         cin >> choiceToAttack;
         if (choiceToAttack > 0 && choiceToAttack < fightersAlive.size() && fightersAlive.at(choiceToAttack)->getHealth() > 0 && fightersAlive.at(choiceToAttack)->getName() == "Enemy"){
             validChoice = true;
         }
-        cout << "Invalid Choice." << endl;
+        cout << "Invalid Choice." << endl; //FIXME: we need to make this more descriptive.
     }
     return choiceToAttack;
 }
@@ -79,6 +88,9 @@ void Combat::removePlayerFromHeap(string targetName) {
     for(int i = 0; i < fightersAlive.size();i++){
         if(fightersAlive.at(i)->getName() == targetName){
             swap(fightersAlive.at(i),fightersAlive.back());
+            //comment by arya:
+            //FIXME: wouldnt popback cause dangling pointers?
+            //       if so, we can use unique_ptr to take care of that
             fightersAlive.pop_back();
         }
     }

07e7db342d16425c1cc3a4ef41293ddc49fb85a4
JSimjee <jameelsimjee@gmail.com>
2024-12-06 15:55:06 -0800

    test-map

--- Tests/Test-Map.cpp
+++ Tests/Test-Map.cpp
@@ -2,6 +2,12 @@
 #include "../googletest/googletest/include/gtest/gtest.h"
 
 TEST(TestMap, MapConstructor) {
-  Map testMap;
+  Map testMap(Weapon::WeaponType::Sword);
+  // Testing that enemies exist in 4 rooms
+  for (unsigned i = 0; i < 16; i++) {
+    vector<Character*> enemiesPerRoom = testMap.returnEnemiesInRoom(i);
 
+    for (unsigned i = 0; i < 2; i++) {
+      
+    }
 }

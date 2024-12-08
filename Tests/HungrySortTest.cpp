1bcd8046b87cb040a3687d72ad5878f3d8d59e35
macos052 <macos052@ucr.edu>
2024-12-08 13:46:48 -0800

    current fixes

--- Tests/HungrySortTest.cpp
+++ Tests/HungrySortTest.cpp
@@ -71,107 +71,283 @@ TEST(SortTest, TwoItemsNotSortedInDescendingOrderTest){
 }
 
 TEST(InsertionSortTest, multipleAscendingItemTest){
-    vector<ItemStack*> stacks = {new ItemStack(new MockItem(ItemType::POTION, "Emilly"))};
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emily's Elixir")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Knight's Shield")));
+    unique_ptr<Item> item;
 
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Healing Tonic")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Excalibur")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Mana Brew")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Thunder Hammer")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Elven Cloak")));
+    item.reset(new MockItem(ItemType::POTION, "Emilly"));
 
-    vector<ItemStack*> results;
-    results.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor")));
-    results.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword")));
-    results.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Elven Cloak")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emilly")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emily's Elixir")));
-    results.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Excalibur")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Healing Tonic")));
-    results.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Knight's Shield")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Mana Brew")));
-    results.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Thunder Hammer")));
+    vector<unique_ptr<ItemStack>> stacks;
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emily's Elixir"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Knight's Shield"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Healing Tonic"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Mana Brew"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Thunder Hammer"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Elven Cloak"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    vector<unique_ptr<ItemStack>> results;
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Elven Cloak"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emilly"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emily's Elixir"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Healing Tonic"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Knight's Shield"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Mana Brew"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Thunder Hammer"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
     InsertionSort s(CompareBy::Name);
     s.sort(stacks, SortOrder::Ascending);
+    s.sort(results, SortOrder::Ascending);
 
     ostringstream out;
     ostringstream result;
     EXPECT_TRUE(s.isSorted(stacks, SortOrder::Ascending));
-    for(ItemStack*& stack : stacks){
-        out << *stack << endl;
-        delete stack;
-        stack = nullptr;
+    for(unsigned i = 0; i < stacks.size(); i++){
+        out << *stacks[i];
     }
-    for(ItemStack*& stack : results){
-        result << *stack << endl;
-        delete stack;
-        stack = nullptr;
+    for(unsigned i = 0; i < results.size(); i++){
+        out << *results[i];
     }
     EXPECT_EQ(out.str(), result.str());
     
 }
 
 TEST(InsertionSortTest, multipleDescendingItemTest){
-    vector<ItemStack*> stacks = {new ItemStack(new MockItem(ItemType::POTION, "Emilly"))};
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emily's Elixir")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Knight's Shield")));
+    unique_ptr<Item> item;
 
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Healing Tonic")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Excalibur")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Mana Brew")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Thunder Hammer")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Elven Cloak")));
+    item.reset(new MockItem(ItemType::POTION, "Emilly"));
 
-    vector<ItemStack*> results;
-    results.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor")));
-    results.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword")));
-    results.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Elven Cloak")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emilly")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emily's Elixir")));
-    results.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Excalibur")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Healing Tonic")));
-    results.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Knight's Shield")));
-    results.push_back(new ItemStack(new MockItem(ItemType::POTION, "Mana Brew")));
-    results.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Thunder Hammer")));
+    vector<unique_ptr<ItemStack>> stacks;
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emily's Elixir"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Knight's Shield"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Healing Tonic"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Mana Brew"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Thunder Hammer"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Elven Cloak"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    vector<unique_ptr<ItemStack>> results;
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Elven Cloak"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emilly"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emily's Elixir"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Healing Tonic"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Knight's Shield"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Mana Brew"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Thunder Hammer"));
+
+    results.push_back(make_unique<ItemStack>(move(item)));
     InsertionSort s(CompareBy::Name);
     s.sort(stacks, SortOrder::Descending);
 
     ostringstream out;
     ostringstream result;
     EXPECT_TRUE(s.isSorted(stacks, SortOrder::Descending));
-    for(ItemStack*& stack : stacks){
-        out << *stack << endl;
-        delete stack;
-        stack = nullptr;
+
+    for(unsigned i = 0; i < stacks.size(); i++){
+        out << *stacks[i] << endl;
     }
-    for(int i = results.size() - 1; i >= 0; i --){
-        result << *results[i] << endl;
-        delete results[i];
-        results[i] = nullptr;
+    for(unsigned i = 0; i < results.size(); i++){
+        out << *results[i] << endl;
     }
+
     EXPECT_EQ(out.str(), result.str());
     
 }
 
 TEST(InsertionSortTest, ascendingTimeSort){
-    vector<ItemStack*> stacks = {new ItemStack(new MockItem(ItemType::POTION, "Emilly"))};
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emily's Elixir")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Knight's Shield")));
+    unique_ptr<Item> item;
 
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Healing Tonic")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Excalibur")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Mana Brew")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Thunder Hammer")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Elven Cloak")));
+    item.reset(new MockItem(ItemType::POTION, "Emilly"));
 
-    vector<ItemStack*> results = stacks;
+    vector<unique_ptr<ItemStack>> stacks;
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emily's Elixir"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Knight's Shield"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Healing Tonic"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Mana Brew"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Thunder Hammer"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Elven Cloak"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    vector<unique_ptr<ItemStack>> results = stacks;
     
     InsertionSort s(CompareBy::Time);
     s.sort(stacks, SortOrder::Ascending);
@@ -179,36 +355,69 @@ TEST(InsertionSortTest, ascendingTimeSort){
     ostringstream out;
     ostringstream result;
     EXPECT_TRUE(s.isSorted(stacks, SortOrder::Ascending));
-    for(ItemStack*& stack : stacks){
-        out << *stack << endl;
+    for(unsigned i = 0; i < stacks.size(); i++){
+        out << *stacks[i] << endl;
     }
-    for(ItemStack*& stack : results){
-        result << *stack << endl;
+    for(unsigned i = 0; i < results.size(); i++){
+        result << *results[i] << endl;
     }
     EXPECT_EQ(out.str(), result.str());
-    for(ItemStack*& stack : stacks){
-        delete stack;
-        stack = nullptr;
-    }
-    for(ItemStack*& stack : results){
-        stack = nullptr;
-    }
 }
 
 TEST(InsertionSortTest, descendingTimeSort){
-    vector<ItemStack*> stacks = {new ItemStack(new MockItem(ItemType::POTION, "Emilly"))};
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Emily's Elixir")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Knight's Shield")));
+    unique_ptr<Item> item;
 
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Healing Tonic")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Excalibur")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::POTION, "Mana Brew")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::WEAPON, "Thunder Hammer")));
-    stacks.push_back(new ItemStack(new MockItem(ItemType::ARMOUR, "Elven Cloak")));
+    item.reset(new MockItem(ItemType::POTION, "Emilly"));
 
-    vector<ItemStack*> results = stacks;
+    vector<unique_ptr<ItemStack>> stacks;
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Emily's Elixir"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Dragon Slayer Sword"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Knight's Shield"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Healing Tonic"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Excalibur"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Dragon Scale Armor"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::POTION, "Mana Brew"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::WEAPON, "Thunder Hammer"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    item.reset(new MockItem(ItemType::ARMOUR, "Elven Cloak"));
+
+    stacks.push_back(make_unique<ItemStack>(move(item)));
+
+    vector<unique_ptr<ItemStack>> results = stacks;
     
     InsertionSort s(CompareBy::Time);
     s.sort(stacks, SortOrder::Descending);
@@ -216,20 +425,13 @@ TEST(InsertionSortTest, descendingTimeSort){
     ostringstream out;
     ostringstream result;
     EXPECT_TRUE(s.isSorted(stacks, SortOrder::Descending));
-    for(ItemStack* stack : stacks){
-        out << *stack << endl;
+    for(unsigned i = 0; i < stacks.size(); i++){
+        out << *stacks[i] << endl;
     }
-    for(int i = results.size() - 1; i >= 0; i--){
+    for(unsigned i = 0; i < results.size(); i++){
         result << *results[i] << endl;
     }
     EXPECT_EQ(out.str(), result.str());
-    for(ItemStack*& stack : stacks){
-        delete stack;
-        stack = nullptr;
-    }
-    for(ItemStack*& stack : results){
-        stack = nullptr;
-    }
 }
 
 TEST(MergeSortTest, multipleAscendingItemTest){
